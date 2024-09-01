068962a2e41798c09b6b3d13dc0b843a8c0c94b8
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-01 11:26:29 +0100

    added files via upload

--- songs/perfect girl/events.hx
+++ songs/perfect girl/events.hx
@@ -1,3 +1,24 @@
+import flixel.addons.display.FlxBackdrop;
+
+var idk = new FlxBackdrop(null, FlxAxes.X);
+var walkDown:Bool = false;
+
 function postCreate() {
-    insert(0, getImageFromGithub("stages/bocchibg"));
+    getImageFromGithub(idk, "stages/bocchibg");
+    insert(0, idk);
+    idk.scrollFactor.x = idk.scrollFactor.y = 0.5;
+    idk.scale.set(0.5, 0.5);
+    idk.y -= 300;
+}
+
+function update(elapsed) {
+    idk.x += 60 * elapsed;
+}
+
+function beatHit(curBeat:Int) {
+    if (curBeat % 2 == 0)  {
+            walkDown = !walkDown;
+            strumLines.members[1].characters[0].y = (walkDown ? 165 : 185);
+            strumLines.members[0].characters[0].y = (walkDown ? 135 : 150);
+    }
 }
