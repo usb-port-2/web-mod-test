9e89240421589368269144b99633dabf88a85c5a
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-01 12:45:14 +0100

    oops

--- songs/popular/script.hx
+++ songs/popular/script.hx
@@ -1,5 +1,12 @@
+import flixel.graphics.frames.FlxAtlasFrames;
+import haxe.xml.Access;
+
 var idk:Int = 0;
 var guh:Float = 0;
+var floatAmount:Float = 25;
+
+var bg = new FunkinSprite();
+var fg = new FunkinSprite();
 
 function postCreate() {
     iconP1.setIcon("tomoko");
@@ -7,16 +14,38 @@ function postCreate() {
     iconP2.visible = false;
     healthBar.percent = health;
     strumLines.members[0].characters[0].x += 350;
+
+    getImageFromGithub(bg, "stages/watamote/bg");
+    insert(0, bg.screenCenter());
+    bg.x += 275;
+    bg.y += 50;
+
+    getImageFromGithub(fg, "stages/watamote/fg");
+    add(fg.screenCenter());
+
+    strumLines.members[0].characters[0].scale.set(0.65, 0.65);
+    strumLines.members[0].characters[0].y += 285;
+    strumLines.members[0].characters[0].x += 40;
+}
+
+function stepHit(curStep:Int) {
+    switch(curStep) {
+        case 0:
+            FlxTween.num(25, 12.5, (Conductor.stepCrochet / 1000) * 256, {}, function(a) floatAmount = a);
+        case 504:
+            FlxTween.num(12.5, 0, (Conductor.stepCrochet / 1000) * 4, {}, function(a) floatAmount = a);
+    }
 }
 
 function update(elapsed) {
-    iconP1.x = healthBar.x + (healthBar.width * (FlxMath.remapToRange(healthBar.percent, 100, 0, 1, 0)) - 75);
-
 	for (a in strumLines.members[0])
 		a.y = idk + Math.sin((Conductor.songPosition / 500) + (a.ID * 2) * 50) * guh;
 
-	guh = FlxMath.lerp(guh, 12.5, 0.0025);
+	guh = FlxMath.lerp(guh, floatAmount, 0.0025);
 }
 
 function onStrumCreation(e) idk = strumLines.members[e.player].startingPos.y;
-function onCameraMove(e) e.position = strumLines.members[0].characters[0].getMidpoint();
+function onCameraMove(e) {
+    e.position.x = bg.getMidpoint().x;
+    e.position.y = bg.getMidpoint().y;
+}
--- songs/the great punishment/events.hx
+++
@@ -1,5 +0,0 @@
-function onStrumCreation(e)
-    e.sprite = "game/notes/gorefield";
-
-function onNoteCreation(e)
-    e.noteSprite = "game/notes/gorefield";
