9c17e75fd260800e26a6a5a5d57afdd547b12b35
~ Care <126361810+usb-port-2@users.noreply.github.com>
2024-09-01 11:55:02 +0100

    freddy fazbear got me pregnant

--- songs/Script.hx
+++ songs/Script.hx
@@ -1,1 +1,6 @@
-trace("my penis hard");
+function postCreate()
+    camGame.fade(FlxColor.BLACK, 0);
+
+function beatHit(curBeat:Int)
+    if(curBeat == 0)
+        camGame.fade(FlxColor.BLACK, (Conductor.crochet/1000) * 4, true);
--- songs/perfect girl/events.hx
+++ songs/perfect girl/events.hx
@@ -6,19 +6,17 @@ var walkDown:Bool = false;
 function postCreate() {
     getImageFromGithub(idk, "stages/bocchibg");
     insert(0, idk);
-    idk.scrollFactor.x = idk.scrollFactor.y = 0.5;
-    idk.scale.set(0.5, 0.5);
-    idk.y -= 300;
+    idk.scrollFactor.x = 0.75;
 }
 
-function update(elapsed) {
-    idk.x += 60 * elapsed;
-}
+function update(elapsed) idk.x += 100 * elapsed;
 
 function beatHit(curBeat:Int) {
     if (curBeat % 2 == 0)  {
             walkDown = !walkDown;
-            strumLines.members[1].characters[0].y = (walkDown ? 165 : 185);
-            strumLines.members[0].characters[0].y = (walkDown ? 135 : 150);
+            strumLines.members[1].characters[0].y = (walkDown ? 215 : 235);
+            strumLines.members[0].characters[0].y = (walkDown ? -15 : -30);
     }
 }
+
+function onCameraMove(e) e.position.y = idk.y + idk.height/2;
